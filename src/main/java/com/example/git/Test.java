43db362d8655530480f30cc17463bbc0cb9f1194
guoshuai <guoshuai@atominds.com>
2019-05-20 12:27:33 +0800

    上传测试

---
+++ src/main/java/com/example/git/Test.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @author GuoShuai
+ * @date 2019-05-20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
