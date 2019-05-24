01cb7e401bac4db334ad17aecb04e02b5eb0d844
guoshuai <guoshuai@atominds.com>
2019-05-24 11:11:17 +0800

    git新分支创建测试

---
+++ src/main/java/com/example/git/NewBranch_Test.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @author GuoShuai
+ * @date 2019-05-24
+ */
+public class NewBranch_Test {
+    public static void main(String[] args) {
+        System.out.println("新分支测试");
+    }
+}
